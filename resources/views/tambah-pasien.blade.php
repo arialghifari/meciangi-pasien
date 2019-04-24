6ebb616227af4abc99aa67068aba6386d54b35d2
Ari algifari <arialghifari99@gmail.com>
2019-04-25 00:05:27 +0800

    form add patient is added

--- resources/views/tambah-pasien.blade.php
+++ resources/views/tambah-pasien.blade.php
@@ -4,14 +4,49 @@
 
 @section('content')
 
-<form action="" class="col-md-3 mx-auto">
-    <div class="form-group">
-        <label for="nama">Nama</label>
-        <input type="text" id="nama" class="form-control">
+<form action="{{-- PR --}}" method="post">
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <label for="nama">Nama</label>
+            <input type="text" id="nama" class="form-control">
+        </div>
     </div>
-    <div class="form-group">
-        <label for="tempat-lahir">Tempat Lahir</label>
-        <input type="text" id="tempat-lahir" class="form-control">
+    <div class="row justify-content-center">
+        <div class="col-md-2">
+            <label for="tempat">Tempat Lahir</label>
+            <input type="text" id="tempat" class="form-control">
+        </div>
+        <div class="col-md-2">
+            <label for="tanggal-lahir">Tanggal Lahir</label>
+            <input type="date" id="tanggal-lahir" class="form-control">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <label for="alamat">Alamat</label>
+            <input type="text" id="alamat" class="form-control">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-4">
+            <label for="no-hp">No Hp</label>
+            <input type="number" id="no-hp" class="form-control">
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col-md-2">
+            <label for="tanggal-masuk-rs">Tanggal Masuk Rs</label>
+            <input type="date" id="tanggal-masuk-rs" class="form-control">
+        </div>
+        <div class="col-md-2">
+            <label for="foto-pasien">Foto Pasien</label>
+            <input type="file" id="foto-pasien" class="form-control" style="heigt:50px;">
+        </div>
+    </div>
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-4">
+            <button type="submit" class="btn btn-primary">Tambah Pasien</button>
+        </div>
     </div>
 </form>
 
